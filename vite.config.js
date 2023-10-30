47efa31837569ca1dc59945388bc0c30343d0362
notakshitgoyal <akshitgoyal92051@gmail.com>
2023-10-30 21:14:53 +0530

    34

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/akshitgoyal",
+  base: "./akshitgoyal",
   plugins: [react()],
   css: {
     postcss: {
